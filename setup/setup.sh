dfaac0b04ccf3cfc1556126118b5c4c09abad0b5
mayankRoyGecko <mayank.roy@geckorobotics.com>
2021-08-19 13:17:05 -0400

    added steam installation to setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,5 @@
 echo "Would you like to setup the virtualenv?(y/n)"
 read setup_venv
-
 if [ $setup_venv == 'y' ] || [ $setup_venv == 'Y' ] 
 then
     . ./setup_virtualenv.sh
@@ -9,6 +8,14 @@ else
 fi
 
 echo "Installing vive_ros2 dependencies."
-
 pip3 install -r requirements.txt
 sudo apt-get install libsdl2-dev
+
+echo "Would you like to install steam? (y/n)"
+read install_steam
+if [ $install_steam == 'y' ] || [ $install_steam == 'Y' ]
+then
+    sudo apt-get install steam
+    echo "Please verify steam was installed correctly. Press enter to continue."
+    read pass
+fi
--- setup/setup_virtualenv.sh
+++ setup/setup_virtualenv.sh
@@ -3,3 +3,5 @@ sudo apt-get install python3-virtualenv
 virtualenv --version
 virtualenv -p /usr/bin/python3 ~/dev/.venv/py3ros2
 source ~/dev/.venv/py3ros2/bin/activate
+echo "Pleas verify the virtial environment was setup correctly. Press enter to continue."
+read pass
