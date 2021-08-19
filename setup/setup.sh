44603798f2b1a4a04e63b79643bf092230f92cdd
mayankRoyGecko <mayank.roy@geckorobotics.com>
2021-08-19 13:45:37 -0400

    added steam install modification to the scripts.

--- setup/setup.sh
+++ setup/setup.sh
@@ -18,4 +18,15 @@ then
     sudo apt-get install steam
     echo "Please verify steam was installed correctly. Press enter to continue."
     read pass
+    echo "Please run steam, update, login and install steamVR. Then press enter to continue."
+    read pass
+    cd ~/.steam/steam/steamapps/common/SteamVR/resources/settings/
+    echo 'Please make the following changes to the default.vrsettings file, at path $cd ~/.steam/steam/steamapps/common/SteamVR/resources/settings/default.vrsettings - '
+    echo "requiredHmd: false"
+    echo "powerOffOnExit: false"
+    echo "turnOffScreensTimeout: 500000"
+    echo "turnOffControllersTimeout: 300000"
+    echo "Press enter when ready to make changes."
+    read pass
+    vi ./default.vrsettings
 fi
--- setup/setup_virtualenv.sh
+++ setup/setup_virtualenv.sh
@@ -2,6 +2,7 @@ sudo apt-get install python3-pip
 sudo apt-get install python3-virtualenv
 virtualenv --version
 virtualenv -p /usr/bin/python3 ~/dev/.venv/py3ros2
+echo "source ~/dev/.venv/py3ros2/bin/activate">> ~/.bashrc
 source ~/dev/.venv/py3ros2/bin/activate
 echo "Pleas verify the virtial environment was setup correctly. Press enter to continue."
 read pass
